b17e4d1f090fbc9f52000ebf4c70aebe69b4bf10
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-06-10 11:07:21 +0700

    add linier queue

--- circuralQueues.cpp
+++ circuralQueues.cpp
@@ -73,5 +73,14 @@ public:
         cout << "\nElements in the queue are.....\n";
 
         // jika Front_position <= REAR_position, iterasu=i dari front hingga rear 
+
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
     }
 };
